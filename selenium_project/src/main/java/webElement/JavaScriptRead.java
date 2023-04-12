d24db0b03bb26f459e460fb515ea5572d48e4d37
SHIYAZ AL SULAIMAN <SHIYAZ AL SULAIMAN@DESKTOP-977KMD5>
2023-04-12 09:03:28 +0530

    commmit

---
+++ selenium_project/src/main/java/webElement/JavaScriptRead.java
@@ -0,0 +1,30 @@
+package webElement;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class JavaScriptRead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeOptions ops = new ChromeOptions(); 
+		ops.addArguments("--remote-allow-origins=*");
+		
+		WebDriver driver = new ChromeDriver(ops);
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeAsyncScript("window.scrollBy(0,1500)", ""); //page scroll vertical pixel position is 1000
+		WebElement dummy=driver.findElement(By.id("dummyid"));
+		js.executeScript("arguments[0].click();", "dummy");
+		
+		
+
+	}
+
+}
--- selenium_project/src/main/java/webElement/Synchronization.java
+++ selenium_project/src/main/java/webElement/Synchronization.java
@@ -1,50 +1,70 @@
 package webElement;
 
 import java.time.Duration;
+import java.util.function.Function;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Synchronization {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		ChromeOptions ops = new ChromeOptions(); 
-		ops.addArguments("--remote-allow-origins=*");
-		
-		WebDriver driver = new ChromeDriver(ops);
-		driver.get("https://www.qabible.in/payrollapp/site/login");
 //		3 types wait 
 //		Implicit Wait - WebDriver wait till page complete loads (High level verification), Only one time declare because its comming from driver
-//		Explicit Wait - WebDriver wait till apecified element loads
-//		Fluent Wait - 
+//		Explicit Wait - WebDriver wait till a Specified element loads
+//		Fluent Wait - polling interval time for specified time upto maximum time out
+		ChromeOptions ops = new ChromeOptions();
+		ops.addArguments("--remote-allow-origins=*");
+
+		WebDriver driver = new ChromeDriver(ops);
+		driver.get("https://www.qabible.in/payrollapp/site/login");
+
 //		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000)); //Select first implicitWait(Selenium 4),Duration is Java class
 		driver.manage().window().maximize();
-		
-		WebElement username=driver.findElement(By.id("loginform-username"));
+
+		WebElement username = driver.findElement(By.id("loginform-username"));
 		username.sendKeys("carol");
-		
-		WebElement password=driver.findElement(By.id("loginform-password"));
+
+		WebElement password = driver.findElement(By.id("loginform-password"));
 		password.sendKeys("1q2w3e4r");
-		
-		WebElement clickSubmit=driver.findElement(By.xpath("//button[@name='login-button']"));
+
+		WebElement clickSubmit = driver.findElement(By.xpath("//button[@name='login-button']"));
 		clickSubmit.click();
-		
-		WebDriverWait wait=new WebDriverWait(driver, Duration.ofMillis(5000)); //Explicit Wait
-		wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.linkText("Clients"))); //expected condition is static so direct call the inbuilt presence method
-		WebElement clientsButton=driver.findElement(By.linkText("Clients"));
+
+		// EXPLICIT WAIT
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(5000)); // Explicit Wait,presence of element
+		wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.linkText("Clients"))); // expected condition is
+																								// static so direct call
+																								// the inbuilt presence
+																								// method
+//		wait.until(ExpectedConditions.textToBe(By.xpath(""), "text value")); // text
+//		wait.until(ExpectedConditions.alertIsPresent());//wait for alert present
+//		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("")));
+		WebElement clientsButton = driver.findElement(By.linkText("Clients"));
 		clientsButton.click();
 
-		
-		
-		
+//		FLUENT WAIT
+		Wait<WebDriver> fluentWait = new FluentWait<WebDriver>(driver)
+			       .withTimeout(Duration.ofSeconds(30L)) //maximum time out
+			       .pollingEvery(Duration.ofSeconds(5L)) //check every  5 seconds/polling interval
+			       .ignoring(NoSuchElementException.class);
+
+			   WebElement foo = fluentWait.until(new Function<WebDriver, WebElement>() {
+			     public WebElement apply(WebDriver driver) {
+			       return driver.findElement(By.id("foo"));
+			     }
+			   });
+
 	}
 
 }
